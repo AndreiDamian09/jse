76a26dc8f4082125874d7020eab8e82d4ef74e89
1ITScrip1 <buzatoiudamian@gmail.com>
2025-03-21 08:51:58 +0200

    end of s04

--- seminar/seminar4/src/ro/ase/pmj/classes/Main.java
+++ seminar/seminar4/src/ro/ase/pmj/classes/Main.java
@@ -1,5 +1,8 @@
 package ro.ase.pmj.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args ){
@@ -11,6 +14,33 @@ public class Main {
         Student[] students=new Student[2];
         Employee[] employees=new Employee[5];
 
+        ArrayList studentLists=new ArrayList();
+        studentLists.add(new Student("balaurentiu",new float[]{10,9.9f,8}));
+        studentLists.add(new Student("balaurentiu",new float[]{10,9.9f,8}));
+        studentLists.add(new Student("balaurentiu",new float[]{10,9.9f,8}));
+        studentLists.add(1,new Student("balaurentiu17",new float[]{10,9.9f,8}));
 
+        ArrayList studentList2= new ArrayList();
+        studentList2.addAll(studentLists);
+
+        System.out.println(studentLists.get(0));
+        System.out.println(studentLists.get(1));
+
+        System.out.println("\n\n");
+
+        if(studentLists.contains(new Student("balaurentiu17",new float[]{10,9.9f,8})) == true){
+            System.out.println("l- am gasit");
+        }else{
+            System.out.println("nu l am gasit");
+        }
+
+        List<Integer> intList=new ArrayList<>();
+        intList.add(1);
+
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+
+        for(Integer i : intList) System.out.println(i );
     }
 }
--- seminar/seminar4/src/ro/ase/pmj/classes/Student.java
+++ seminar/seminar4/src/ro/ase/pmj/classes/Student.java
@@ -1,5 +1,7 @@
 package ro.ase.pmj.classes;
 
+import java.util.Arrays;
+
 public class Student {
 
     public String name;
@@ -8,5 +10,17 @@ public class Student {
     public Student(String name, float[] grades) {
         this.name = name;
         this.grades = grades;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        return this.name ==((Student)obj).name;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", grades=" + Arrays.toString(grades);
     }
 }
